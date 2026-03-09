9d9a92d0985e06c57314d0cf815ff91399eda90b
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-09 21:14:15 +0800

    setting up the for loop in item 7

---
+++ programming_challenge_batch_1/python_practice_program_batch_1_item_7.py
@@ -0,0 +1,4 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+numbers = []
+for i in range(10):
+    number = float(input(f"Enter 10 numbers: "))   
