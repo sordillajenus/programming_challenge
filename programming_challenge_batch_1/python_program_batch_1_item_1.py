df747d8400be8ada18f986b4c29ed83a0e437b53
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-09 20:57:39 +0800

    first item for batch one done (program that asks to enter two numbers and it will display the bigger number)

--- programming_challenge_batch_1/python_program_batch_1_item_1.py
+++ programming_challenge_batch_1/python_program_batch_1_item_1.py
@@ -1,1 +1,9 @@
-print("hello world")
+# Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+if first_number > second_number:
+    print(f"The bigger number is: {first_number}")
+elif second_number > first_number:
+    print(f"The bigger number is: {second_number}")
+else:
+    print("Both numbers are equal. Please enter different numbers.")
