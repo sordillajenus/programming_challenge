f7dfc2a4cc41f31b6b46b8c72fbe5ea7313f0f5f
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-09 21:00:47 +0800

    2nd item done for batch one (the program asks to enter two valus and check whether they are equal or not)

---
+++ programming_challenge_batch_1/python_program_batch_1_item_2.py
@@ -0,0 +1,7 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.  
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+if first_number == second_number:
+    print("Equal")
+else:
+    print("Not Equal") 
