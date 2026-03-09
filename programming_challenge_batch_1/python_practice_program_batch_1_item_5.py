b2a6beece076b290dafc611341b57c6a5419db6c
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-09 21:11:19 +0800

    item 5 for batch one is done (program that prints the quotient of the two numbers)

---
+++ programming_challenge_batch_1/python_practice_program_batch_1_item_5.py
@@ -0,0 +1,5 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+first_number = float(input("Enter the first number: ")) 
+second_number = float(input("Enter the second number: "))
+quotient = first_number / second_number
+print(f"The quotient of the two numbers is: {quotient}")
