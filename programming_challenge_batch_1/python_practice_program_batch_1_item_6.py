a293f9594f84535bd050e78e16f0c4590da4e7ab
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-09 21:13:01 +0800

    item 6 is done (program that raises the first number to the second number according to the input of the user)

---
+++ programming_challenge_batch_1/python_practice_program_batch_1_item_6.py
@@ -0,0 +1,5 @@
+# Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+result = first_number ** second_number
+print(f"The result when the first number is raised to the second number is: {result}")
