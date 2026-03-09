f7ca45b3deaa3adbe5463fe1cfc609df7f66d9e2
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-09 21:04:58 +0800

    3rd item done (program that displays the sum of the two numbers)

---
+++ programming_challenge_batch_1/python_program_batch_1_item_3.py
@@ -0,0 +1,5 @@
+# Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.    
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number:"))
+sum_of_numbers = first_number + second_number
+print(f"The sum of the two numbers is: {sum_of_numbers}")
