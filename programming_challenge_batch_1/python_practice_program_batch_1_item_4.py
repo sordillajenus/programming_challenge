cec1e1ec212262e6788747418996d9bf3359411d
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-09 21:08:53 +0800

    item 4 done (program that prints the product of the two numbers) and also i renamed the python files

---
+++ programming_challenge_batch_1/python_practice_program_batch_1_item_4.py
@@ -0,0 +1,5 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+product_of_the_two_numbers = first_number * second_number
+print(f"The product of the two numbers is: {product_of_the_two_numbers}")
