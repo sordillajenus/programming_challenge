bc981923ab6e6ef8e1d197d592d229b01ace85ad
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-12 23:39:05 +0800

    item 2 for batch 2 done (python program that displays "not equal" when the user entered a non-equal values)

---
+++ programming_challenge_batch_2/python_program_challenge_batch_2_item_2.py
@@ -0,0 +1,8 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num1 != num2:
+    print("Not Equal")
+else:
+    print("Equal")
