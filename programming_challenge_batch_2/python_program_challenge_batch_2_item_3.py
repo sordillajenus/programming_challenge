f2e33900a2ab2a620a9c05a259d2c596991e4ae2
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-12 23:41:22 +0800

    item 3 for batch 2 done (program that displays the difference of two numbers)

---
+++ programming_challenge_batch_2/python_program_challenge_batch_2_item_3.py
@@ -0,0 +1,5 @@
+# Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))    
+difference = num1 - num2
+print("The difference of the two numbers is:", difference)
