1ef01366364944f969971b2b297f0e389692c442
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-18 15:24:09 +0800

    batch 1 item 7 done

--- programming_challenge_batch_1/python_practice_program_batch_1_item_7.py
+++ programming_challenge_batch_1/python_practice_program_batch_1_item_7.py
@@ -1,4 +1,8 @@
-# Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
-numbers = []
-for i in range(10):
-    number = float(input(f"Enter 10 numbers: "))   
+# Prog07: Create a program that asks the user to input 10 numbers and prints the sum of all the numbers.
+total = 0
+
+for i in range(1, 11):
+    number = float(input(f"Enter number {i}: "))
+    total += number
+
+print(f"The sum of all the numbers is: {total}")
