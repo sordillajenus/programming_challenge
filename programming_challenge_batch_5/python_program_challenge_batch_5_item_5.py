0a608c4db3448ebfe23593ca46c1f43f1e3f67ec
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-19 17:09:03 +0800

    item 5 batch 5 (full name in proper casing)

---
+++ programming_challenge_batch_5/python_program_challenge_batch_5_item_5.py
@@ -0,0 +1,3 @@
+# Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+fullname = input("Enter your fullname: ")
+print(fullname.title())
