555af6f5e10724e84864c756e658b8e4e5de6efd
sordillajenus@gmail.com <sordillajenus@gmail.com>
2026-03-19 17:10:40 +0800

    item 6 batch 5(reverse casing)

---
+++ programming_challenge_batch_5/python_program_challenge_batch_5_item_6.py
@@ -0,0 +1,3 @@
+# Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+fullname = input("Enter your fullname: ")
+print(fullname.swapcase())
